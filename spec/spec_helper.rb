b72e37d08a00e4d923581686533e62ccc007b529
Takumi IINO <trot.thunder@gmail.com>
2016-03-22 00:19:25 +0900

    travis, coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,13 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'bundler/setup'
+
+require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
+
 require 'pdflib_mini'
